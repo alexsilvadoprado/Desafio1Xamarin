e06e793e14309f38c5ee3532ea2d2e69706ad248
alexprado <alexsilvadoprado@gmail.com>
2017-11-10 15:30:44 -0300

    Primeiro commit

---
+++ Desafio1/Desafio1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Desafio1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void OnButtonClearClicked(object sender, EventArgs e)
+        {
+            lblNumero.Text = "123";
+        }
+    }
+}
